836f01a2f7e5d61d537853dcce9837a9086fa3ce
pythonwithsean <pythonwithsean@gmail.com>
2024-06-06 17:04:47 +0100

    "Brute Force Solution"

--- Container With Most Water.js
+++ Container With Most Water.js
@@ -3,22 +3,32 @@
 // the width is i max - i min
 // 2 Pointer Solution
 const height = [1,8,6,2,5,4,8,3,7]
-let left = 0 
-let right = height.length
-let max = 0
-while (left < right){
-	if(height[left] < height[right]){
-		left +=1 
-	}else{
-		right -=1
-	}
+let bounds  = height.length
+// let left = 0 
+// let right = height.length
+// let max = 0
+// while (left < right){
+// 	if(height[left] < height[right]){
+// 		left +=1 
+// 	}else{
+// 		right -=1
+// 	}
 	
-	let amount = Math.abs(right - left) * Math.min(height[left],height[right])
-	if(max < amount){
-		max = amount
-	}
+// 	let amount = Math.abs(right - left) * Math.min(height[left],height[right])
+// 	if(max < amount){
+// 		max = amount
+// 	}
 
+// }
+// console.log(max)
+
+// O(N^2) BruteForce Solution
+let max= 0 
+for(let i = 0; i < bounds; i++){
+	for(y = i + 1; y < bounds; y++){
+		let amount = Math.min(height[i], height[y]) * Math.abs(y - i)
+		if(max < amount) max = amount
+	}
 }
 console.log(max)
-
 
