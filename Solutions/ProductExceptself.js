3df59ddc6a1f0ad09741dee911a5def4cffcd4c5
pythonwithsean <pythonwithsean@gmail.com>
2024-06-06 21:53:52 +0100

    "Product ExceptSelf O(1) space Solution "

--- Solutions/ProductExceptself.js
+++ Solutions/ProductExceptself.js
@@ -17,23 +17,44 @@
 			// 	}
 			// }
 			// console.log(res)
+
+			
+// Second Solution
+// let leftArr = new Array(nums.length).fill(1) 
+// leftArr[0] =  1
+// let rightArr = new Array(nums.length).fill(1)
+// rightArr[rightArr.length - 1] = 1
+// for (i = 1; i < leftArr.length; i++){
+	// 	leftArr[i] = nums[i - 1] * leftArr[i -1]
+	// }
+	
+	// for(i = nums.length - 1; i > 0; i--){
+		// 	rightArr[i-1] = rightArr[i] * nums[i]
+		// }
+		// let result = new Array(nums.length).fill(1)
+		// for (let v = 0; v < result.length; v++){
+			// 	result[v] = leftArr[v] * rightArr[v]
+			// }
+			
+			// console.log(result)
+			
 			
 let nums = [1,2,3,4]
-let leftArr = new Array(nums.length).fill(1) 
-leftArr[0] =  1
-let rightArr = new Array(nums.length).fill(1)
-rightArr[rightArr.length - 1] = 1
-for (i = 1; i < leftArr.length; i++){
-	leftArr[i] = nums[i - 1] * leftArr[i -1]
-}
+const length = nums.length
+let result = new Array(length).fill(1)
 
-for(i = nums.length - 1; i > 0; i--){
-	rightArr[i-1] = rightArr[i] * nums[i]
+// Result = [1,1,1,1]
+// Add Left Side of Array
+let LeftProduct = 1
+for (let i = 0; i < length; i++){
+	result[i] = LeftProduct
+	LeftProduct *= nums[i]
 }
-let result = new Array(nums.length).fill(1)
-for (let v = 0; v < result.length; v++){
-	result[v] = leftArr[v] * rightArr[v]
+console.log(result)
+let right_product = 1
+for(let i = length -1; i > -1; i--){
+	result[i] *= right_product
+	right_product *= nums[i]
 }
 
 console.log(result)
-
